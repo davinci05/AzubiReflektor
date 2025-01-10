5b3e54d4c36609798b9e9aa9a6e85f3701526a42
tschiede <tschiede@ford.com>
2025-01-10 13:04:33 +0100

    added public transportation module to config and changed module to StationID and kvb provider

--- config/config.js
+++ config/config.js
@@ -113,6 +113,30 @@ let config = {
 				broadcastNewsUpdates: true
 			}
 		},
+		{
+			module: "MMM-PublicTransportHafas",
+			position: "bottom_right",
+
+			config: {
+				// Departures options
+				stationID: "900000367",                   // Replace with your stationID!
+				stationName: "Köln Niehl Geestemünder Str.", // Replace with your station name!
+				direction: "",                    // Show only departures heading to this station. (A station ID.)
+				excludedTransportationTypes: [],  // Which transportation types should not be shown on the mirror? (comma-separated list of types) possible values: "tram", "bus", "suburban", "subway", "regional" and "national"
+				ignoredLines: [],                 // Which lines should be ignored? (comma-separated list of line names)
+				timeToStation: 10,                // How long do you need to walk to the next Station?
+
+				// Look and Feel
+				displayLastUpdate: true,          // Display the last time of module update.
+				maxUnreachableDepartures: 0,      // How many unreachable departures should be shown?
+				maxReachableDepartures: 7,        // How many reachable departures should be shown?
+				showColoredLineSymbols: true,     // Want colored line symbols?
+				customLineStyles: "",             // Prefix for the name of the custom css file. ex: Leipzig-lines.css (case sensitive)
+				showOnlyLineNumbers: false,       // Display only the line number instead of the complete name, i. e. "11" instead of "STR 11"
+				showTableHeadersAsSymbols: true,  // Table Headers as symbols or text?
+				useColorForRealtimeInfo: true     // Want colored real time information (timeToStation, early)?
+			}
+		},
 	]
 };
 
