ddae68419af6122d5feeeb2c38737b99aaa0f6a6
LMOOREN <lmooren@ford.com>
2025-01-23 10:09:16 +0100

    Minor changes to mm.sh script

--- mm.sh
+++ mm.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# change this to the directory where the AzubiReflektor is located
+APP_DIR="/home/admin/AzubiReflektor"
+
 echo "Starting AzubiReflektor..."
-/usr/bin/npm run start --prefix /home/admin/AzubiReflektor 
+/usr/bin/npm run start --prefix $APP_DIR
